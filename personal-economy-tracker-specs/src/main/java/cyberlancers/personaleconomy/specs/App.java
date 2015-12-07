91b9d5ece6479ebaf16cae907e3155a68d854d3e
AffanHasan <affanhasan88@gmail.com>
2015-12-07 13:03:46 +0500

    Added specs

---
+++ personal-economy-tracker-specs/src/main/java/cyberlancers/personaleconomy/specs/App.java
@@ -0,0 +1,13 @@
+package cyberlancers.personaleconomy.specs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ personal-economy-tracker-specs/src/main/resources/specifications/expense-logging.feature
@@ -0,0 +1,6 @@
+Feature: Category wise expense logging
+
+	Scenario: Scenario 1
+		Given I have done following shopping today:
+			| Fruits | Vegetables | Dairy  | 
+			| 70 PKR | 100 PKR	  | 30 PKR |
